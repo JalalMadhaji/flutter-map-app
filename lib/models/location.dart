6aa54a8b979f494da266d415907f542e6bfdfe96
JalalMadhaji <jalal.almadhaji90@gmail.com>
2021-10-26 04:47:31 +0300

    Create location.dart

---
+++ lib/models/location.dart
@@ -0,0 +1,14 @@
+class Location {
+  int id;
+  double lat;
+  double long;
+  String title;
+  String description;
+  String phone;
+  String ageAverage;
+  String workDays;
+  String dailyHours;
+
+  Location(this.id, this.lat, this.long, this.title, this.description,
+      this.phone, this.ageAverage, this.workDays, this.dailyHours);
+}
