60d0c1db5d2658a70e95e1f9509de9270e683de4
JalalMadhaji <jalal.almadhaji90@gmail.com>
2021-10-28 04:11:21 +0300

    Update login.dart

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map_app/ui/screens/register.dart';
-import 'package:flutter_map_app/utils/constants.dart';
+import 'package:flutter_map_app/controllers/main_controller.dart';
+import 'package:flutter_map_app/ui/widgets/edit_text_item.dart';
+import 'package:flutter_map_app/ui/widgets/header_item.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,7 +14,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _name = TextEditingController();
   final TextEditingController _password = TextEditingController();
-  bool isLoading = false;
 
   final _formKey = GlobalKey<FormState>();
 
@@ -25,93 +26,40 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: const Center(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                  ),
-                ),
-              ),
+            const HeaderItem(
+              title: "Login",
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                controller: _name,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                    borderSide:
-                        const BorderSide(color: Colors.black, width: .5),
-                  ),
-                  label: const Text("User Name"),
-                ),
-                validator: (value) {
-                  if (value == null || value != kLoginKey) {
-                    return "Please enter user name correctly!";
-                  }
-                  return null;
-                },
-              ),
+            CustomEditText(
+              controller: _name,
+              isObscureText: false,
+              placeholder: "User Name",
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                controller: _password,
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.password),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                    borderSide:
-                        const BorderSide(color: Colors.black, width: .5),
-                  ),
-                  label: const Text("Password"),
-                ),
-                validator: (value) {
-                  if (value == null || value != kLoginKey) {
-                    return "Please enter password correctly!";
-                  }
-                  return null;
-                },
-              ),
+            CustomEditText(
+              controller: _password,
+              isObscureText: true,
+              placeholder: "Password",
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      await Future.delayed(const Duration(seconds: 2));
-                      setState(() {
-                        isLoading = false;
-                      });
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterPage()),
-                      );
-                    }
-                  },
-                  child: isLoading
-                      ? const CircularProgressIndicator(
-                          color: Colors.white,
-                        )
-                      : const Text("Login"),
-                ),
-              ),
+            Consumer<MainController>(
+              builder: (context, model, child) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await model.handleLoginClick(
+                            _formKey.currentState!.validate(), context);
+                      },
+                      child: model.isLoading
+                          ? const CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : const Text("Login"),
+                    ),
+                  ),
+                );
+              },
             )
           ],
         ),
